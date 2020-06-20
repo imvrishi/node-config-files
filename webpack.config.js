815ce8a40eaca543f00af3f0341beceb1b3a794c
Rishikesh Vishwakarma <imvrishi@gmail.com>
2020-06-20 18:51:29 +0530

    Added typescript and more configs

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    watchContentBase: true,
+    clientLogLevel: "error",
+    noInfo: true,
+    open: false,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
